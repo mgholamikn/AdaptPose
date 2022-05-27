fc417f35fc8c716ab6f7d858289d455d62b709d9
mgholamikn <55782887+mgholamikn@users.noreply.github.com>
2022-05-26 20:45:04 -0700

    Create pretrained_model.sh

---
+++ scripts/pretrained_model.sh
@@ -0,0 +1,12 @@
+cd checkpoint/adaptpose/videopose/gt/3dhp/
+gdown --id 11QJf-B5D1_aqsMR3v-9rrfht4W7ErC4k
+cd ../../../../..
+cd checkpoint/adaptpose/videopose/gt/3dpw/
+gdown --id 1LUi1OQ7vWQ5KXJ9HlSvtveiOEacG1WoS
+cd ../../../../..
+cd checkpoint/adaptpose/videopose/gt/skii/
+gdown --id 1b1_A7SuBdGaPNXzsx2qLOg0_RqIuk6Cx
+cd ../../../../..
+cd checkpoint/pretrain_baseline/videopose/gt/3dhp/
+gdown --id 1SmSXrk_LcpsXBpBEsSp0zq9qIpb4aJBU
+cd ../../../../..
