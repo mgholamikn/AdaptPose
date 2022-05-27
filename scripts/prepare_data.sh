4c0d9aaf9750512f804b8862b34cbacc46d7513e
mgholamikn <55782887+mgholamikn@users.noreply.github.com>
2022-05-26 17:45:56 -0700

    Create prepare_data.sh

---
+++ scripts/prepare_data.sh
@@ -0,0 +1,11 @@
+#%cd /TO/AdaptPose/ 
+mkdir -p data
+cd data
+## download data
+gdown --id 1_cJ2vG5wxRQUtWavz47XAwaMvU9pQB_X
+gdown --id 1NX9dT1nKX-8t_eZR3vnfrdyOegG7pDBs
+gdown --id 1v3-LDnjlHT8OJi40E7OKDBGY7dWM9P2n
+gdown --id 12voJG5DBFy_2xT3lNdLYA4ZCTYjs7Iyv
+gdown --id 1Y96GlQOEkc2Gx6V4FpvTwCyJjrUX3FYu
+gdown --id 1vL53iJ1mWao3TKK3p52ZX_jdF5d5EBtl
+gdown --id 1EbVV-nzrrQ2KdT_GG4JPwv1A7EOnFka1
